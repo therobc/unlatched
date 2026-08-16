b7107de1cb5cf6efef435e8f9bd21b397be9fe75
therobc <317077900+therobc@users.noreply.github.com>
2026-08-15 23:42:49 -0400

    Add 1 files to packaging

--- packaging/build_release.py
+++ packaging/build_release.py
@@ -38,7 +38,7 @@ import zipfile
 from pathlib import Path
 
 APP_NAME = "Unlatched"
-APP_VERSION = "0.1.2"
+APP_VERSION = "0.1.3"
 
 REPO_ROOT = Path(__file__).resolve().parent.parent  # app/
 PACKAGING_DIR = REPO_ROOT / "packaging"
